2f058c1e5097f55b71c89dabb6833575c94d5ac1
Cody Caro <codycaro@Codys-MacBook-Pro.local>
2019-10-16 20:17:57 -0700

    commented out build folder in gitignore

---
+++ art-portfolio/build/precache-manifest.17cf7cbb575886fee50a2c73fe1a7269.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fa9de70635812ec7cd4ef554f94666c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c2fef0a836debb4bc80",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "40332c5587e744347f13",
+    "url": "/static/js/2.f417fe93.chunk.js"
+  },
+  {
+    "revision": "1c2fef0a836debb4bc80",
+    "url": "/static/js/main.09f50f9d.chunk.js"
+  },
+  {
+    "revision": "b8a1880a5d562bce800e",
+    "url": "/static/js/runtime-main.efdc333d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
