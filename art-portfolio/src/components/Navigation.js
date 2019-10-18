4a8c44bdff79b8f57f46b1f2deeb38be4c557654
Toniddarden1 <Toniddarden@gmail.com>
2019-10-17 22:56:14 -0400

    added route + dummy file routes

---
+++ art-portfolio/src/components/Navigation.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavLink, Route } from "react-router-dom";
+import styled from "styled-components";
+import AboutUs from "./About";
+import App from "../App";
+import SignIn from "./SignIn";
+
+const Navigation = () => {
+  return (
+    <nav>
+      <NavLink to={`/`}>home</NavLink>
+      <NavLink to={`/about`}>About</NavLink>
+      <NavLink to={`/signIn`}>Sign-In</NavLink>
+
+      {/* <Route exact path="/" component={App} />
+
+      <Route exact path="/about" component={AboutUs} />
+
+      <Route exact path="/signIn" component={SignIn} /> */}
+    </nav>
+  );
+};
+
+export default Navigation;
