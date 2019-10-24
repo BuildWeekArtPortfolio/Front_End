a05100c44dfac76004b433d4036dfe40f9d97a61
Allie Robinson <allierob93@gmail.com>
2019-10-23 21:38:38 -0500

    Took out extra code

--- art-portfolio/src/App.js
+++ art-portfolio/src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Route } from 'react-router-dom';
 import Navigation from "./components/Navigation";
-import AboutUs from "./components/About";
-import SignIn from "./components/SignIn";
 import Routes from "./routes/Routes";
 
 function App() {
